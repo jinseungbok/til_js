ea6249041782d76877ee92e2764182f6e6404a30
jinseungbok <92.bokjs@gmail.com>
2025-05-13 13:19:18 +0900

    [docs]:function_250513

--- lib.js
+++ lib.js
@@ -1,12 +1,9 @@
-```js
 /**
- * 나이를 10살 더하여서 출력함.
- * @param {number} age - 현재 나이 입력
- * ```javascript
- * const result = showAge(10); // 20
- * ```
+ * 너비를 계산해 주는 함수
+ * @param {number} _width
+ * @param {number} _height
+ * @returns {number}
  */
-function showAge(age) {
-  return age + 10;
+function calcRect(_width, _height) {
+  return _width * _height;
 }
-```
